48004fddd14a081ff0aab1f7ef8837c097b49f6c
Kento75 <kento2github@gmail.com>
2018-11-19 12:04:23 +0900

    Add .gitattributes

--- kento75_blog/urls.py
+++ kento75_blog/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('<int:pk>/delete', BlogDeleteView.as_view(), name='delete'),  # 記事削除画面
     path('<int:pk>', BlogDetailView.as_view(), name='detail'),         # 記事詳細画面
     path('create', BlogCreateView.as_view(), name='create'),           # 記事投稿画面
-    path('login', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login', LoginView.as_view(template_name='login.html'), name='login'),      # ログイン画面
+    path('logout', LogoutView.as_view(template_name='logout.html'), name='logout')
     path('admin/', admin.site.urls),
 ]
