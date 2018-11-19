80ba390ca6020e63ec1dedcbd8450c72bb4fb6d4
Kento75 <kento2github@gmail.com>
2018-11-19 18:14:24 +0900

    Add access test codes

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_access_success(self):
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code, 'status code == 200')
+
+    def test_index_access_failed(self):
+        response = self.client.get('/test')
+        self.assertEqual(404, response.status_code, 'status code == 404')
